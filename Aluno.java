c22bb04b03680b6e0d62db88a853a9c2e420fdb8
erico-lt <erico.blp@gmail.com>
2022-06-05 19:34:21 -0300

    Criançaão da classe Aluno

---
+++ Aluno.java
@@ -0,0 +1,47 @@
+public class Aluno extends Pessoa{
+    private String curso;
+    private boolean matricula;
+
+    public Aluno(String nome, String sexo, String curso, int idade){
+        this.setNome(nome);
+        this.setSexo(sexo);
+        this.setCurso(curso);
+        this.setIdade(idade);
+        this.setMatricula(true);
+    }
+
+    // metodos para cancelar e renovar matricula
+    public void cancelarMatricula(){
+        this.setMatricula(false);
+    }
+    public void renovarMatricular(){
+        this.setMatricula(true);
+    }   
+    
+    /*/ metodos "get e set" para modificar e 
+        retornar o status da matricula e curso
+     */
+    public String getCurso() {
+        return curso;
+    }
+   
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public boolean getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(boolean matricula){
+        this.matricula = matricula;
+    }
+
+    public String toString(){
+        return "Nome: " + this.getNome() + 
+        "/n Sexo: " + this.getSexo() + 
+        "/n Idade: " + this.getIdade() +
+        "/n Curso: " + this.getCurso() + 
+        "/n Matricula: " + this.getMatricula();
+    }
+}
