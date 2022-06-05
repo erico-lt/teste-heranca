65b7d4111c553d3ac0448a22814d9bdaad73ab12
erico-lt <erico.blp@gmail.com>
2022-06-05 11:35:07 -0300

    Criação da Classe pessoa

--- Pessoa.java
+++ Pessoa.java
@@ -1,5 +1,33 @@
-public class Pessoa{
-    public static void main(String[] args){
-        
+public class Pessoa{    
+    private String nome;
+    private String sexo;
+    private int idade;
+
+    public void fazerAniversario(){
+        this.setIdade(1);
     }
+    
+    public String getNome() {
+        return this.nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getSexo() {
+        return this.sexo;
+    }
+    
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade){
+        this.idade += idade;
+    }           
 }
